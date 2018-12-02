68781ac27a65854feacfc22d75add3d80112ca95
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-12-02 15:29:53 +0100

    :art: Improve Components Appearance

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,11 +1,15 @@
 import h from 'hyperapp-style' // eslint-disable-line
 
 const Header = (props, children) => (
-  <h1 style={{ ...props.style, fontFamily: 'Roboto Slab' }}>{children}</h1>
+  <h1 style={{ ...props.style, fontFamily: 'Roboto Slab' }}>
+    {children}
+  </h1>
 )
 
 const Flex = (props, children) => (
-  <div style={{ ...props.style, display: 'flex' }}>{children}</div>
+  <div style={{ ...props.style, display: 'flex' }}>
+    {children}
+  </div>
 )
 
 export { Header, Flex }
